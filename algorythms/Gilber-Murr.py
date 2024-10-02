44a51afc8e554e1a694841aec68f12313c0203cc
v.serov.2022 <v.serov.2022@stud.nstu.ru>
2024-10-02 13:33:26 +0700

    remake of functions

--- algorythms/Gilber-Murr.py
+++ algorythms/Gilber-Murr.py
@@ -4,8 +4,22 @@ from fractions import Fraction
 class Gilber_Murr:
 
 
-    def __init__(self):
-        pass
+    def __init__(self, probs: dict):
+        self.probs = probs
+        self.mas_coding = {}
+        q, sigma, l, summa_lenghts, entropia, self.kraft = 0, 0, 0, 0, 0, 0
+        for i in p.keys():
+            entropia -= float(Fraction(p[i])) * math.log(float(Fraction(p[i])), 2)
+            sigma = q + float(Fraction(p[i])) / 2
+            q += float(Fraction(p[i]))
+            l = int((-math.log(float(Fraction(p[i])) / 2, 2)) + 0.999999)
+            summa_lenghts += l
+            duble_number = self.decimal_to_binary_float(sigma, l)
+            mas_coding[i] = duble_number
+            self.kraft += 2 ** (-l)
+        self.middle_lenght = summa_lenghts / len(p)
+        redundancy = middle_lenght - entropia
+
     # Перевод числа с плавающей точкой в двочиную систему счисления
     def decimal_to_binary_float(self,number, dl):
         res = ''
@@ -19,86 +33,70 @@ class Gilber_Murr:
         return res
 
     # Алгоритм для кодирования последовательности
-    def coding(self):
-        with open('in1.txt', encoding='utf-8') as input_file1, open('in2.txt', encoding='utf-8') as input_file2:
-            p = [i.strip().split() for i in input_file1.readlines()]
-            stroka = input_file2.read().strip()
-            mas_coding = {}
-            q, sigma, l, summa_lenghts, entropia, Kraft = 0, 0, 0, 0, 0, 0
+    def encoding(self, sequence: str):
 
-            for i in range(len(p)):
-                entropia -= float(Fraction(p[i][1])) * math.log(float(Fraction(p[i][1])), 2)
-                sigma = q + float(Fraction(p[i][1])) / 2
-                q += float(Fraction(p[i][1]))
-                l = int((-math.log(float(Fraction(p[i][1])) / 2, 2)) + 0.999999)
-                summa_lenghts += l
-                duble_number = decimal_to_binary_float(sigma, l)
-                mas_coding[p[i][0]] = duble_number
-                Kraft += 2 ** (-l)
+        p = self.probs
 
-            middle_lenght = summa_lenghts / len(p)
-            redundancy = middle_lenght - entropia
-            for key, value in mas_coding.items():
-                print(f'Символ: \'{key}\' кодируется \'{value}\'')
-            print(f'''Средняя длина: {middle_lenght}
-    Избыточность: {redundancy}
-    Крафт: {Kraft}''')
-            with open('in3.txt', 'w', encoding='utf-8') as output_file:
-                res = ''
-                for i in stroka:
-                    for j in mas_coding:
-                        if str(i) == str(j):
-                            res += mas_coding[i]
-                print(res, file=output_file)
-                print('Результат кодирования выведен в файл')
+
+
+
+
+        for key, value in mas_coding.items():
+            print(f'Символ: \'{key}\' кодируется \'{value}\'')
+        print(f'Средняя длина: {middle_lenght}\n'
+               f'Избыточность: {redundancy}\n'
+                f'Крафт: {Kraft}')
+        res = ''
+        for i in sequence:
+            for j in mas_coding:
+                if str(i) == str(j):
+                    res += mas_coding[i]
+
+        return res, mas_coding, middle_lenght, redundancy, Kraft
 
 
     # Алгоритм для декодирования последовательности
-    def decoding(self):
-        with open('in1.txt', encoding='utf-8') as input_file1, open('in3.txt', encoding='utf-8') as input_file2:
-            p = [i.strip().split() for i in input_file1.readlines()]
-            stroka = input_file2.read().strip()
-            mas_coding = {}
-            q, sigma, l, summa_lenghts, entropia, Kraft = 0, 0, 0, 0, 0, 0
+    def decoding(self,sequence: str):
+        p = self.probs
+        q, sigma, l, summa_lenghts, entropia, Kraft = 0, 0, 0, 0, 0, 0
 
-            for i in range(len(p)):
-                entropia -= float(Fraction(p[i][1])) * math.log(float(Fraction(p[i][1])), 2)
-                sigma = q + float(Fraction(p[i][1])) / 2
-                q += float(Fraction(p[i][1]))
-                l = int((-math.log(float(Fraction(p[i][1])) / 2, 2)) + 0.999999)
-                summa_lenghts += l
-                duble_number = decimal_to_binary_float(sigma, l)
-                mas_coding[p[i][0]] = duble_number
-                Kraft += 2 ** (-l)
+        for i in p.keys():
+            entropia -= float(Fraction(p[i])) * math.log(float(Fraction(p[i])), 2)
+            sigma = q + float(Fraction(p[i])) / 2
+            q += float(Fraction(p[i]))
+            l = int((-math.log(float(Fraction(p[i])) / 2, 2)) + 0.999999)
+            summa_lenghts += l
+            duble_number = self.decimal_to_binary_float(sigma, l)
+            mas_coding[i] = duble_number
+            Kraft += 2 ** (-l)
 
-            middle_lenght = summa_lenghts / len(p)
-            redundancy = middle_lenght - entropia
-            for key, value in mas_coding.items():
-                print(f'Символ: \'{key}\' кодируется \'{value}\'')
-            print(f'''Средняя длина: {middle_lenght}
-    Избыточность: {redundancy}
-    Крафт: {Kraft}''')
-            with open('in3.txt', 'w', encoding='utf-8') as output_file:
-                res = ''
-                a = len(min(mas_coding.values(), key=len))
-                b = len(max(mas_coding.values(), key=len))
-                while stroka:
-                    for i in range(a, b + 1):
-                        flag = True
-                        current = stroka[:i]
-                        for key, value in mas_coding.items():
-                            if str(current) == str(value):
-                                res += key
-                                stroka = stroka[i:]
-                                flag = False
-                                break
-                        if not flag:
-                            break
-                print(res, file=output_file)
-                print('Результат декодирования выведен в файл')
+        middle_lenght = summa_lenghts / len(p)
+        redundancy = middle_lenght - entropia
+        for key, value in mas_coding.items():
+            print(f'Символ: \'{key}\' кодируется \'{value}\'')
+        print(f'Средняя длина: {middle_lenght}\n Избыточность: {redundancy}\n Крафт: {Kraft}')
+        res = ''
+        a = len(min(mas_coding.values(), key=len))
+        b = len(max(mas_coding.values(), key=len))
+        while sequence:
+            for i in range(a, b + 1):
+                flag = True
+                current = sequence[:i]
+                for key, value in mas_coding.items():
+                    if str(current) == str(value):
+                        res += key
+                        sequence = sequence[i:]
+                        flag = False
+                        break
+                if not flag:
+                    break
+        return res, mas_coding, middle_lenght, redundancy, Kraft
 
 if __name__ == "__main__":
-    g_m = Gilber_Murr
+    probs = {'+':'0.2','-':'0.2','*':'0.2','/':'0.2','=':'0.2'}
+    seq = '++--**//=='
+    seq1 = '0001000101000100100010001011101111101110'
+    g_m = Gilber_Murr(probs=probs)
     # Основная программа
     end = 1
     while end == 1:
@@ -106,27 +104,27 @@ if __name__ == "__main__":
     1 - Кодирование последовательности
     2 - Декодирование последовательности\n'''))
         if choice == 1:
-            # with open('in1.txt', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
+            # with open('in1.csv', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
             #     symbol1 = print(f'+ {float(Fraction(input('Введите вероятность для символа \'+\' ')))}', file=in1)
             #     symbol2 = print(f'- {float(Fraction(input('Введите вероятность для символа \'-\' ')))}', file=in1)
             #     symbol3 = print(f'* {float(Fraction(input('Введите вероятность для символа \'*\' ')))}', file=in1)
             #     symbol4 = print(f'/ {float(Fraction(input('Введите вероятность для символа \'/\' ')))}', file=in1)
             #     symbol5 = print(f'= {float(Fraction(input('Введите вероятность для символа \'=\' ')))}', file=in1)
             #     posled = print(f'{input('Введите строку для кодирования(декодирования)\n')}', file=in2)
-            g_m.coding()
+            g_m.encoding(seq)
             end = int(input('''Выбери действие
     1 - Выбрать следующее действие
     Другое - Остановить программу\n'''))
 
         elif choice == 2:
-            # with open('in1.txt', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
+            # with open('in1.csv', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
             #     symbol1 = print(f'+ {float(Fraction(input('Введите вероятность для символа \'+\' ')))}', file=in1)
             #     symbol2 = print(f'- {float(Fraction(input('Введите вероятность для символа \'-\' ')))}', file=in1)
             #     symbol3 = print(f'* {float(Fraction(input('Введите вероятность для символа \'*\' ')))}', file=in1)
             #     symbol4 = print(f'/ {float(Fraction(input('Введите вероятность для символа \'/\' ')))}', file=in1)
             #     symbol5 = print(f'= {float(Fraction(input('Введите вероятность для символа \'=\' ')))}', file=in1)
             #     posled = print(f'{input('Введите строку для кодирования(декодирования)\n')}', file=in2)
-            g_m.decoding()
+            g_m.decoding(seq1)
             end = int(input('''Выбери действие
     1 - Выбрать следующее действие
     Другое - Остановить программу\n'''))
