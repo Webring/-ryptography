6785a096291eb87dd23ca2a711ac5c6a71cf0751
zmandot <v.serov.2022@stud.nstu.ru>
2024-10-02 15:50:14 +0700

    Rewritten Gilber-Murr algorythm

--- algorythms/Gilber-Murr.py
+++ algorythms/Gilber-Murr.py
@@ -1,27 +1,27 @@
 import math
 from fractions import Fraction
 
-class Gilber_Murr:
 
+class Gilber_Murr:
 
     def __init__(self, probs: dict):
         self.probs = probs
         self.mas_coding = {}
         q, sigma, l, summa_lenghts, entropia, self.kraft = 0, 0, 0, 0, 0, 0
-        for i in p.keys():
-            entropia -= float(Fraction(p[i])) * math.log(float(Fraction(p[i])), 2)
-            sigma = q + float(Fraction(p[i])) / 2
-            q += float(Fraction(p[i]))
-            l = int((-math.log(float(Fraction(p[i])) / 2, 2)) + 0.999999)
+        for i in probs.keys():
+            entropia -= float(Fraction(probs[i])) * math.log(float(Fraction(probs[i])), 2)
+            sigma = q + float(Fraction(probs[i])) / 2
+            q += float(Fraction(probs[i]))
+            l = int((-math.log(float(Fraction(probs[i])) / 2, 2)) + 0.999999)
             summa_lenghts += l
             duble_number = self.decimal_to_binary_float(sigma, l)
-            mas_coding[i] = duble_number
+            self.mas_coding[i] = duble_number
             self.kraft += 2 ** (-l)
-        self.middle_lenght = summa_lenghts / len(p)
-        redundancy = middle_lenght - entropia
+        self.middle_lenght = summa_lenghts / len(probs)
+        self.redundancy = self.middle_lenght - entropia
 
     # Перевод числа с плавающей точкой в двочиную систему счисления
-    def decimal_to_binary_float(self,number, dl):
+    def decimal_to_binary_float(self, number, dl):
         res = ''
 
         for _ in range(dl):
@@ -35,54 +35,33 @@ class Gilber_Murr:
     # Алгоритм для кодирования последовательности
     def encoding(self, sequence: str):
 
-        p = self.probs
-
-
-
-
-
-        for key, value in mas_coding.items():
+        for key, value in self.mas_coding.items():
             print(f'Символ: \'{key}\' кодируется \'{value}\'')
-        print(f'Средняя длина: {middle_lenght}\n'
-               f'Избыточность: {redundancy}\n'
-                f'Крафт: {Kraft}')
+        print(f'Средняя длина: {self.middle_lenght}\n'
+              f'Избыточность: {self.redundancy}\n'
+              f'Крафт: {self.kraft}')
         res = ''
         for i in sequence:
-            for j in mas_coding:
+            for j in self.mas_coding:
                 if str(i) == str(j):
-                    res += mas_coding[i]
+                    res += self.mas_coding[i]
 
-        return res, mas_coding, middle_lenght, redundancy, Kraft
-
+        return res, self.mas_coding, self.middle_lenght, self.redundancy, self.kraft
 
     # Алгоритм для декодирования последовательности
-    def decoding(self,sequence: str):
-        p = self.probs
-        q, sigma, l, summa_lenghts, entropia, Kraft = 0, 0, 0, 0, 0, 0
+    def decoding(self, sequence: str):
 
-        for i in p.keys():
-            entropia -= float(Fraction(p[i])) * math.log(float(Fraction(p[i])), 2)
-            sigma = q + float(Fraction(p[i])) / 2
-            q += float(Fraction(p[i]))
-            l = int((-math.log(float(Fraction(p[i])) / 2, 2)) + 0.999999)
-            summa_lenghts += l
-            duble_number = self.decimal_to_binary_float(sigma, l)
-            mas_coding[i] = duble_number
-            Kraft += 2 ** (-l)
-
-        middle_lenght = summa_lenghts / len(p)
-        redundancy = middle_lenght - entropia
-        for key, value in mas_coding.items():
+        for key, value in self.mas_coding.items():
             print(f'Символ: \'{key}\' кодируется \'{value}\'')
-        print(f'Средняя длина: {middle_lenght}\n Избыточность: {redundancy}\n Крафт: {Kraft}')
+        print(f'Средняя длина: {self.middle_lenght}\n Избыточность: {self.redundancy}\n Крафт: {self.kraft}')
         res = ''
-        a = len(min(mas_coding.values(), key=len))
-        b = len(max(mas_coding.values(), key=len))
+        a = len(min(self.mas_coding.values(), key=len))
+        b = len(max(self.mas_coding.values(), key=len))
         while sequence:
             for i in range(a, b + 1):
                 flag = True
                 current = sequence[:i]
-                for key, value in mas_coding.items():
+                for key, value in self.mas_coding.items():
                     if str(current) == str(value):
                         res += key
                         sequence = sequence[i:]
@@ -90,10 +69,11 @@ class Gilber_Murr:
                         break
                 if not flag:
                     break
-        return res, mas_coding, middle_lenght, redundancy, Kraft
+        return res, self.mas_coding, self.middle_lenght, self.redundancy, self.kraft
+
 
 if __name__ == "__main__":
-    probs = {'+':'0.2','-':'0.2','*':'0.2','/':'0.2','=':'0.2'}
+    probs = {'+': '0.2', '-': '0.2', '*': '0.2', '/': '0.2', '=': '0.2'}
     seq = '++--**//=='
     seq1 = '0001000101000100100010001011101111101110'
     g_m = Gilber_Murr(probs=probs)
@@ -104,26 +84,12 @@ if __name__ == "__main__":
     1 - Кодирование последовательности
     2 - Декодирование последовательности\n'''))
         if choice == 1:
-            # with open('in1.csv', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
-            #     symbol1 = print(f'+ {float(Fraction(input('Введите вероятность для символа \'+\' ')))}', file=in1)
-            #     symbol2 = print(f'- {float(Fraction(input('Введите вероятность для символа \'-\' ')))}', file=in1)
-            #     symbol3 = print(f'* {float(Fraction(input('Введите вероятность для символа \'*\' ')))}', file=in1)
-            #     symbol4 = print(f'/ {float(Fraction(input('Введите вероятность для символа \'/\' ')))}', file=in1)
-            #     symbol5 = print(f'= {float(Fraction(input('Введите вероятность для символа \'=\' ')))}', file=in1)
-            #     posled = print(f'{input('Введите строку для кодирования(декодирования)\n')}', file=in2)
             g_m.encoding(seq)
             end = int(input('''Выбери действие
     1 - Выбрать следующее действие
     Другое - Остановить программу\n'''))
 
         elif choice == 2:
-            # with open('in1.csv', 'w', encoding='utf-8') as in1, open('in2.txt', 'w', encoding='utf-8') as in2:
-            #     symbol1 = print(f'+ {float(Fraction(input('Введите вероятность для символа \'+\' ')))}', file=in1)
-            #     symbol2 = print(f'- {float(Fraction(input('Введите вероятность для символа \'-\' ')))}', file=in1)
-            #     symbol3 = print(f'* {float(Fraction(input('Введите вероятность для символа \'*\' ')))}', file=in1)
-            #     symbol4 = print(f'/ {float(Fraction(input('Введите вероятность для символа \'/\' ')))}', file=in1)
-            #     symbol5 = print(f'= {float(Fraction(input('Введите вероятность для символа \'=\' ')))}', file=in1)
-            #     posled = print(f'{input('Введите строку для кодирования(декодирования)\n')}', file=in2)
             g_m.decoding(seq1)
             end = int(input('''Выбери действие
     1 - Выбрать следующее действие
