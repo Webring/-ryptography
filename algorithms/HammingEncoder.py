3603028e6019c98b67f4ed69ab889614ef9a25c9
zmandot <v.serov.2022@stud.nstu.ru>
2024-10-15 13:51:47 +0700

    add reading of hamming matrix

--- algorithms/HammingEncoder.py
+++ algorithms/HammingEncoder.py
@@ -4,23 +4,16 @@ import math
 
 
 class HammingCode:
-    def __init__(self, probs: dict, hamming_matrix):
+    def __init__(self, probs: dict):
         # Создаем экземпляр GilbertMooreEncoder
-        self.hamming_matrix = hamming_matrix
+        self.hamming_matrix = None
         self.encoder = GilbertMooreEncoderWithCheck(probs)
-        self.G = self.generate_hamming_matrix()
+        self.generation_matrix = None
         self.codes_for_symbols_GM = self.encoder.codes_for_symbols
         self.codes_hamming = {}
-        for key,value in self.codes_for_symbols_GM.items():
-            value_list = [int(digit) for digit in value]
-            code_with_checkers = self.vector_matrix_mult(value_list, self.G)
-            self.codes_hamming[key] = ''.join(str(num) for num in code_with_checkers)
+
         self.result = ''
         self.distance = []
-        for i in range(len(self.codes_hamming)):
-            self.distance.append([])
-            for j in range(len(self.codes_hamming)):
-                self.distance[i].append(1000)
 
         self.errors = []
         self.sindrome = 0
@@ -37,8 +30,8 @@ class HammingCode:
         n = len(list(self.codes_hamming.values())[0])
         k = len(list(self.codes_for_symbols_GM.values())[0])
         d0 = min(min(row) for row in self.distance)
-        sum_comb_hamming = sum([math.comb(n, i) for i in range(int((d0 - 1) / 2) + 1)])
-        sum_comb_varsh = sum([math.comb(n - 1, i) for i in range(d0 - 2 + 1)])
+        sum_of_combinations_for_hamming = sum([math.comb(n, i) for i in range(int((d0 - 1) / 2) + 1)])
+        sum_of_combinations_for_varshamov = sum([math.comb(n - 1, i) for i in range(d0 - 2 + 1)])
 
         return f'''{'\n'.join([f'Символ: \'{key}\' кодируется \'{value}\'' for key, value in self.codes_hamming.items()])}
 Расстояния Хэмминга:
@@ -46,10 +39,22 @@ class HammingCode:
 Кратсность обнаружения: q_обн <= {math.ceil(d0 / 2)}
 Кратсность исправления: q_исп <= {int((d0 - 1) / 2)}
 Наименьшее расстояние Хэмминга: d0 = {d0}
-Граница Хэмминга: r = n - k = {n - k} >= log2({sum_comb_hamming}) = {math.log2(sum_comb_hamming)} {' Не выполняется' if (math.log2(sum_comb_hamming) > (n - k)) else " Выполняется"}
+Граница Хэмминга: r = n - k = {n - k} >= log2({sum_of_combinations_for_hamming}) = {math.log2(sum_of_combinations_for_hamming)} {' Не выполняется' if (math.log2(sum_of_combinations_for_hamming) > (n - k)) else " Выполняется"}
 Граница Плоткина: d0 = {min(min(row) for row in self.distance)} <= n * 2^(k-1) / (2^k – 1) = {n * pow(2, k - 1) / (pow(2, k) - 1)} {' Не выполняется' if (d0 > n * pow(2, k - 1) / (pow(2, k) - 1)) else " Выполняется"}
-Граница Варшамова-Гильберта: 2^(n-k) = {pow(2, n - k)} > {sum_comb_varsh} {' Не выполняется' if (pow(2, n - k) <= sum_comb_varsh) else " Выполняется"}
+Граница Варшамова-Гильберта: 2^(n-k) = {pow(2, n - k)} > {sum_of_combinations_for_varshamov} {' Не выполняется' if (pow(2, n - k) <= sum_of_combinations_for_varshamov) else " Выполняется"}
 '''
+
+    def read_hamming_matrix(self, matrix):
+        self.hamming_matrix = matrix
+        self.generation_matrix = self.generate_hamming_matrix()
+        for key,value in self.codes_for_symbols_GM.items():
+            value_list = [int(digit) for digit in value]
+            code_with_checkers = self.vector_matrix_mult(value_list, self.generation_matrix)
+            self.codes_hamming[key] = ''.join(str(num) for num in code_with_checkers)
+        for i in range(len(self.codes_hamming)):
+            self.distance.append([])
+            for j in range(len(self.codes_hamming)):
+                self.distance[i].append(1000)
 
 
     def generate_hamming_matrix(self):
@@ -61,8 +66,8 @@ class HammingCode:
 
         # Заполнение генерирующей матрицы
         G[:, :4] = np.eye(4, dtype=int)  # Единичная матрица 4x4
-        H_transposed = np.transpose(self.hamming_matrix)
-        G[:, 4:] = np.array(H_transposed[:4])  # Проверочные биты (из H)
+        hamming_matrix_transposed = np.transpose(self.hamming_matrix)
+        G[:, 4:] = np.array(hamming_matrix_transposed[:4])  # Проверочные биты (из H)
 
         return G
 
@@ -84,7 +89,7 @@ class HammingCode:
             v[position] = 0
         else:
             v[position] = 1
-        print(f"Был исправлен бит на {position + 1} позиции")
+        # print(f"Был исправлен бит на {position + 1} позиции")
         return v
 
     def check_and_correct(self, v):
@@ -92,18 +97,18 @@ class HammingCode:
         Проверяет и исправляет ошибки в кодовом слове v.
         """
         hamming_matrix_transposed = np.transpose(self.hamming_matrix)
-        s = self.vector_matrix_mult(v, hamming_matrix_transposed)
+        sindrome_vector = self.vector_matrix_mult(v, hamming_matrix_transposed)
 
 
         for i in range(7):
-            if np.array_equal(s, hamming_matrix_transposed[i]):
+            if np.array_equal(sindrome_vector, hamming_matrix_transposed[i]):
                 if i + 1 == 2:
                     self.sindrome = 2
-                    print(f"Ошибка во 2 бите")
+                    # print(f"Ошибка во 2 бите")
                     v = self.convert(v, i)
                     break
                 self.sindrome = i+1
-                print(f"Ошибка в {i + 1} бите")
+                # print(f"Ошибка в {i + 1} бите")
                 v = self.convert(v, i)
                 break
         return v
@@ -113,6 +118,8 @@ class HammingCode:
                 Кодирует последовательность символов.
                 sequence: str — строка, которую нужно закодировать.
                 """
+        if self.hamming_matrix is None:
+            raise Exception("Матрица Хэмминга не была загружена.\nКодирование невозможно")
         self.result = ''
         for i in sequence:
             if i not in self.codes_hamming.keys():
@@ -137,6 +144,8 @@ class HammingCode:
         Декодирует двоичную последовательность в символы.
         sequence: str — двоичная строка, которую нужно декодировать.
         """
+        if self.hamming_matrix is None:
+            raise Exception("Матрица Хэмминга не была загружена.\nДекодирование невозможно")
         self.result = ''
         # Находим минимальную и максимальную длины кодов.
         minimum_of_length = len(min(self.codes_hamming.values(), key=len))
@@ -144,14 +153,14 @@ class HammingCode:
 
         limit = len(sequence)  # Ограничение по длине последовательности.
         current_index = 0
-        current = ''
+        current_part_of_sequence = ''
         while sequence and limit:
             # Пробегаем от минимальной длины до максимальной.
             for i in range(minimum_of_length, maximum_of_length + 1):
                 flag = True
-                current = sequence[:i]  # Текущий подстрок из последовательности.
+                current_part_of_sequence = sequence[:i]  # Текущий подстрок из последовательности.
                 for key, value in self.codes_hamming.items():
-                    if str(current) == str(value):
+                    if str(current_part_of_sequence) == str(value):
                         # Если найден соответствующий код, добавляем символ в результат.
                         self.result += key
                         sequence = sequence[i:]  # Убираем закодированную часть из последовательности.
@@ -161,7 +170,7 @@ class HammingCode:
                 if not flag:
                     break
             if limit < len(sequence)/2:
-                current_list = [int(digit) for digit in current]
+                current_list = [int(digit) for digit in current_part_of_sequence]
                 correct_vector = self.check_and_correct(current_list)
                 self.errors.append(f"word: {current_index+1}, bit: {self.sindrome}")
                 self.result += ''.join(str(num) for num in correct_vector)
@@ -179,19 +188,19 @@ if __name__ == "__main__":
     ]
     probs = {'+': '0.2', '-': '0.2', '*': '0.2', '/': '0.2', '=': '0.2'}
 
-    hamming_code = HammingCode(hamming_matrix=H, probs=probs)
-
+    hamming_code = HammingCode(probs=probs)
+    hamming_code.read_hamming_matrix(H)
     hamming_code.encode('+-/')
-    print(hamming_code.result)
-    print(hamming_code.check_and_correct([0,0,0,1,1,0,1]))
+    # print(hamming_code.result)
+    # print(hamming_code.check_and_correct([0,0,0,1,1,0,1]))
 
     hamming_code.decode('000110101011111011000')
-    print(hamming_code.errors)
+    # print(hamming_code.errors)
     # Кодовое слово
     # code = [0, 0, 1, 1]
     #
     # # Генерация кода
-    # v = hamming_code.vector_matrix_mult(code, hamming_code.G)
+    # v = hamming_code.vector_matrix_mult(code, hamming_code.generation_matrix)
     # print(f"Закодированное слово: {v}")
     #
     # # Эмулируем ошибку
@@ -202,4 +211,4 @@ if __name__ == "__main__":
     # corrected_vector = hamming_code.check_and_correct(v)
     # print(f"Исправленное слово: {corrected_vector}")
 
-    print(str(hamming_code))
+    # print(str(hamming_code))
