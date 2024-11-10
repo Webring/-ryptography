01b436a9cca0662d511494bea0d952e9d1dee744
zmandot <v.serov.2022@stud.nstu.ru>
2024-11-10 17:40:17 +0700

    updated EichenauerLehnGeneratorWithReversal.py

--- algorithms/EichenauerLehnGeneratorWithReversal.py
+++ algorithms/EichenauerLehnGeneratorWithReversal.py
@@ -2,86 +2,86 @@ from scipy.stats import chisquare
 from collections import Counter
 
 
-# Функция для нахождения обратного элемента по модулю
-def reverse_to_mod(x, n):
-    for i in range(1, n):
-        if (x * i) % n == 1:
-            return i
-    return None
+class EichenauerLehnGeneratorWithReversal:
+    def __init__(self, N, a, c, x_0):
+        self.N = N
+        self.a = a
+        self.c = c
+        self.x_0 = x_0
+        self.p_value = 0
+        self.alpha = 0.05
+        self.mas = []
+        self.output_text = ''
+        if not self.validation():
+            raise Exception("Входные данные были заданы неверно")
 
+    def __repr__(self):
+        return f'EichenauerLehnGeneratorWithReversal(N = {self.N},a = {self.a},c = {self.c},x_0 = {self.x_0})'
 
-# Функция для генерации последовательности
-def generate(x, a, c, n):
-    if x == 0:
-        return c
-    else:
-        result = (reverse_to_mod(x, n) * a + c) % n
-    return result
+    def __str__(self):
+        return f'{self.output_text}\n {'Последовательность отклоняется от ожидаемого распределения (H0 отвергается).' if self.p_value < self.alpha
+        else "Нет оснований отвергать нулевую гипотезу (H0): последовательность соответствует ожидаемому распределению."}'
 
+    def validation(self):
+        return 0 <= self.a < self.N and 0 <= self.c < self.N and 0 <= self.x_0 < self.N
 
-# Функция для проверки входных данных
-def validation(a, c, x, n):
-    return 0 <= a < n and 0 <= c < n and 0 <= x < n
+    def reverse_to_mod(self, x):
+        for i in range(1, self.N):
+            if (x * i) % self.N == 1:
+                return i
+        return None
 
-# Функция для подсчета периода
-def period(array):
-    point = array[0]
-    k = 0
-    dl = 0
-    for i in array:
-        if i == point:
-            dl += 1
-        if dl == 2:
-            break
-        k += 1
-    return k
-
-
-# Функция для проверки качества сгенерированной последовательности
-def check_sequence_quality(sequence):
-    length = len(sequence)
-    # Подсчет частоты значений
-    observed_counts = Counter(sequence)
-    # Ожидаемая частота
-    expected_count = length / len(set(sequence))
-    expected_counts = {value: expected_count for value in observed_counts.keys()}
-    # Сбор значений и частот для теста Пирсона
-    observed_values = []
-    expected_values = []
-    for value in sorted(expected_counts.keys()):
-        observed_values.append(observed_counts.get(value, 0))
-        expected_values.append(expected_counts[value])
-    # Применение критерия согласия Пирсона
-    8
-    chi2_stat, p_value = chisquare(observed_values, expected_values)
-    alpha = 0.05  # уровень значимости
-    if p_value < alpha:
-        return 'Последовательность отклоняется от ожидаемого распределения (H0 отвергается).'
-    else:
-        return "Нет оснований отвергать нулевую гипотезу (H0): последовательность соответствует ожидаемому распределению."
-
-
-# Функция для запуска генерации последовательности и вывода результатов
-def run_generation():
-    try:
-        N = int(input("Введите N: "))
-        a = int(input("Введите a: "))
-        c = int(input("Введите c: "))
-        x0 = int(input("Введите x0: "))
-        mas = [x0]
-        if validation(a, c, x0, N):
-            for _ in range(N):
-                mas.append(generate(mas[-1], a, c, N))
-            seq_period = period(mas)
-            check = check_sequence_quality(mas)
-            output_text = (f'Последовательность значений: {mas}\n'
-                           f'Период: {seq_period}\n'
-                           f'Пирсон: {check}')
-            print("Результаты", output_text)
+    def generate(self, x):
+        if x == 0:
+            return self.c
         else:
-            print("Ошибка", 'Введены некорректные данные')
-    except ValueError:
-        print("Ошибка", 'Пожалуйста, введите корректные целые числа')
+            result = (self.reverse_to_mod(x) * self.a + self.c) % self.N
+        return result
+
+    def period(self, array):
+        point = array[0]
+        k = 0
+        dl = 0
+        for i in array:
+            if i == point:
+                dl += 1
+            if dl == 2:
+                break
+            k += 1
+        return k
+
+    def check_sequence_quality(self, sequence):
+        length = len(sequence)
+        # Подсчет частоты значений
+        observed_counts = Counter(sequence)
+        # Ожидаемая частота
+        expected_count = length / len(set(sequence))
+        expected_counts = {value: expected_count for value in observed_counts.keys()}
+        # Сбор значений и частот для теста Пирсона
+        observed_values = []
+        expected_values = []
+        for value in sorted(expected_counts.keys()):
+            observed_values.append(observed_counts.get(value, 0))
+            expected_values.append(expected_counts[value])
+        # Применение критерия Пирсона
+        chi2_stat, self.p_value = chisquare(observed_values, expected_values)
+
+    def run_generation(self):
+        self.mas = [self.x_0]
+        for _ in range(self.N):
+            self.mas.append(self.generate(self.mas[-1]))
+            seq_period = self.period(self.mas)
+            self.check_sequence_quality(self.mas)
+            self.output_text = (f'Последовательность значений: {self.mas}\n'
+                                f'Период: {seq_period}\n'
+                                f'Пирсон: ')
+
 
 if __name__ == "__main__":
-    run_generation()
+    N = int(input("Введите N: "))
+    a = int(input("Введите a: "))
+    c = int(input("Введите c: "))
+    x0 = int(input("Введите x0: "))
+    EL = EichenauerLehnGeneratorWithReversal(N, a, c, x0)
+    EL.run_generation()
+    print(str(EL))
